8708b7a5782dd39b296af6aa56d016a5be7bca77
ekratz <kratz2407@gmail.com>
2025-02-12 15:10:57 +0100

    setup for first testcase

--- test/test.py
+++ test/test.py
@@ -1,1 +1,24 @@
+import json
 import unittest
+
+
+class TestFunctions(unittest.TestCase):
+
+    def test_haversine_distance(self):
+        # get testcases from testdata.py
+        testcases =
+
+        # check results for all testcases
+        for case in testcases:
+            input = case["input"]
+            expect_res = case["expected"]
+
+            # get the actual result
+            res =
+
+            # check if results are equal
+            self.assertEqual(res, expect_res,
+                             f"")
+
+if __name__ == "__main__":
+    unittest.main()
